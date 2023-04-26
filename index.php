04ff89f2c35f13536797c96f143d206a42c0453e
WebSowl <131889110+WebSowl@users.noreply.github.com>
2023-04-26 17:28:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Дизельное топливо | Лендинг</title>
+</head>
+<body>
+  <?php include 'content.html'; ?>
+  <script src="main.js"></script>
+</body>
+</html>
